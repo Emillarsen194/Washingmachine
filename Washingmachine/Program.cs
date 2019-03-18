832010a76fe3693a8e048abac7531ae54e2dfd6a
Emillarsen194 <47022984+Emillarsen194@users.noreply.github.com>
2019-03-18 12:06:13 +0100

    Add project files.

---
+++ Washingmachine/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Washingmachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool power = false;
+            Console.WriteLine("Do u wanna turn on the washing machine? yes or no");
+            String yesNo = Console.ReadLine().ToLower() ;
+
+            if (yesNo == "yes")
+            {
+                power = true; // then power is true 
+            }
+
+            else
+            {
+                power = false; // Else dont turn on the washing machine 
+            }
+            Washer washingnow = new Washer(power); //making new washer object with power 
+
+            if (washingnow.power == true) // if washingnow.property power is true 
+            {
+
+
+            Console.WriteLine("what program do u wanna use fill, ecowash, wash");
+            String programs = Console.ReadLine(); //choose program
+
+
+
+
+
+            String whatProgram = "";
+
+                int timer = 0; // our timer to check how long program been running for 
+
+            
+
+            switch (programs)
+            {
+                case "wash":
+                    whatProgram = washingnow.Wash(); //wash method normal program 
+                        timer = washingnow.Timer(whatProgram);
+                    break;
+                case "ecowash":
+                    whatProgram = washingnow.EcoWash(); //Eco was method 
+                        timer = washingnow.Timer(whatProgram);
+                    break;
+                case "fill":
+                    whatProgram = washingnow.Fill(); // fill washing program 
+                        timer = washingnow.Timer(whatProgram);
+                    break;
+            }
+                
+
+
+                    Console.WriteLine(timer); // writes our timer 
+
+                
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Washingmachine/Washer.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Washingmachine
+{
+    class Washer
+    {
+        public bool power;
+        //public bool door;
+        //private bool soapdoor;
+        public String program;
+        private int time;
+
+        public int Time
+        {
+            get
+            {
+                return time;
+            }
+            set
+            {
+                time = value;
+            }
+        }
+
+
+        public Washer()
+        {
+
+        }
+         public Washer(bool onoff)
+        {
+            this.power = onoff;
+      
+        }
+         
+
+
+        public bool PowerOnOff(bool power)
+        {
+            return power;
+        }
+     
+
+        public int Timer(String whatprogram)
+        {
+            
+            int seconds = 0;
+            while (time > seconds)
+            {
+                seconds++;
+                Console.WriteLine("running " + whatprogram + " for: " + seconds + " seconds"); // progrma been running for 
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+
+            if (time <= seconds)
+            {
+                Console.WriteLine(whatprogram + " done running"); // when done running tell done running 
+            }
+
+            power = false; // and turn off power 
+           
+            return seconds; // return seconds 
+
+            
+        }
+
+        //public int UseSoap()
+        //{
+
+        //}
+           
+        //public int UseWater()
+        //{
+
+        //}
+
+        public String Fill()
+        {
+            time = 5; //fill program is only running for 5 seconds 
+
+ 
+            return "program fill"; 
+     
+        }
+
+            
+
+        public String Wash()
+        {
+            time = 10; // wash program running for 5 seconds
+            return "Program Wash";
+        }
+
+        public String EcoWash()
+        {
+            time = 20; // ecowash running for 20 seconds 
+            return "program ecoWash";
+        }
+    }
+}
